61c2601bf6adb0d9237b268f32b970f0abedeadd
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-08-16 10:15:06 -0300

    up

---
+++ Landing_Pages/AZ-coin/Tela_Banco_AZ/config.php
@@ -0,0 +1,14 @@
+<?php
+$dbHost = 'localhost';
+$dbName = 'azcoin';// Mudar para AZMerit !!
+$dbUser = 'postgres';
+$dbPass = '9825@04';
+
+try{
+    $conexao = new PDO("pgsql:host=$dbHost;dbname=" . $dbName, $dbUser, $dbPass);
+    //echo "conexao relizada com sucesso";
+}catch(PDOException $err){
+    //echo "banco não conectado" . " " .$err->getMessage();
+}
+
+?>
