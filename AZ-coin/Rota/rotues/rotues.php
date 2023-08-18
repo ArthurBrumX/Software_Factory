22531d2bf4fad5ad5f227dbad257ae0ae8dcc402
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-08-18 11:11:01 -0300

    up

---
+++ AZ-coin/Rota/rotues/rotues.php
@@ -0,0 +1,11 @@
+<?php
+    $rotues = [
+        "GET" => [
+            "/" => "HomeController",
+            "/contact' => 'ContactController"
+        ],
+        "POST" => [
+            "Contact" => "ContactController",
+        ]
+    ]
+?>
