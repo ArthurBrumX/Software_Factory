ae249a7910fc67b3ebbb4a6512ad85b6fa5e61a8
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-13 12:26:04 -0300

    up

---
+++ FreshTrack/Information.py
@@ -0,0 +1,93 @@
+# Desafio “Resolva o problema com programação” 
+
+# Etapa 1: 
+# Brainstorm. 
+# O grupo deverá em comum acordo encontrar um problema a ser resolvido. 
+# Tempo: 30 min 
+
+# Etapa 2 
+# O grupo deverá confirmar se realmente o problema que escolheram é realmente um 
+# problema, se sim, as partes envolvidas querem que esse problema seja resolvido? 
+# Se não busque outro problema ou adapte o problema anterior. 
+# Dica: Para que o grupo confirme que o problema escolhido é realmente um problema 
+# que precisa ser resolvido, façam "pesquisa de mercado", por meio de perguntas as 
+# partes envolvidas (as perguntas podem ser realizadas via aplicativo de mensagem). 
+# Tempo: 50min 
+
+# Etapa 3: 
+# Com o problema em mãos é hora de buscarem a solução. Discutam como o problema 
+# será resolvido. 
+# Tempo:25min 
+
+# Etapa 4: 
+# Com a solução em mãos, crie o Diagrama de Classes. 
+# Tempo:15min 
+
+# Etapa5: 
+# Implementação da solução em Python 
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Problema:
+
+# Muitas pessoas estão preocupadas com o desperdício de alimentos em suas casas. Muitas vezes, produtos perecíveis são esquecidos no 
+# fundo da geladeira ou despensa e acabam estragando antes de serem usados. Isso não apenas desperdiça comida, mas também dinheiro.
+
+# Nome da Startup: "FreshTrack"
+
+# Descrição:
+
+# FreshTrack é uma plataforma digital que ajuda os usuários a gerenciar e rastrear seus alimentos de forma 
+# eficiente para reduzir o desperdício e economizar dinheiro. A plataforma oferece as seguintes funcionalidades:
+
+# Inventário Inteligente: Os usuários podem digitalizar os códigos de barras dos alimentos ou inserir manualmente os produtos 
+# em sua despensa e geladeira. A plataforma cria automaticamente uma lista de alimentos disponíveis.
+
+# Alertas de Vencimento: FreshTrack monitora as datas de validade dos alimentos e envia alertas para os usuários quando um 
+# produto está prestes a vencer. Isso ajuda os usuários a lembrarem-se de usar itens antes que estraguem.
+
+# Receitas Personalizadas: A plataforma sugere receitas com base nos alimentos disponíveis no inventário do usuário. 
+# Isso ajuda a planejar refeições de forma mais eficaz e a usar alimentos antes que expirem.
+
+# Lista de Compras Inteligente: Com base nas receitas escolhidas e nos itens que estão prestes a vencer, o FreshTrack gera automaticamente uma 
+# lista de compras personalizada para evitar a compra excessiva de alimentos não necessários.
+
+# Doações de Alimentos: A plataforma também permite que os usuários identifiquem alimentos que não serão consumidos a tempo e oferece 
+# opções para doá-los a organizações locais de combate à fome.
+
+# Essa startup aborda o problema do desperdício de alimentos, ajudando os usuários a serem mais conscientes em relação ao que têm 
+# em casa, quando usar os alimentos e como evitar o desperdício. Além disso, ela incentiva a doação de alimentos não consumidos, contribuindo para a redução 
+# do desperdício em nível comunitário.
+
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+# Leitor De Codigo de Barras 
+
+# Caracteristicas:
+    # Leitor De Codigo De Barras - (Para Alimentos Fechados)
+    # Balança ( para Frutas)
+
+
+
+
+# classe cadastro produto
+# classe retirar produto
+# classe adicionar poduto
+
+# funcao para emitir o aleta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
