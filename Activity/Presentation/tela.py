67e1cc0442a5d38ef582ad3e10fd901cfe22cc19
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-20 04:44:54 -0300

    up

--- Activity/Presentation/tela.py
+++ Activity/Presentation/tela.py
@@ -1,23 +1,40 @@
 import PySimpleGUI as sg
+#Importando a biblioteca
 
 class telaPython:
     def __init__(self):
         # layout
         layout = [
-            [sg.Text('Nome'),sg.Input()],
-            [sg.Text('Idade'),sg.Input()],
-            [sg.Checkbox('Gmail'), sg.Checkbox('Outlook'), sg.Checkbox('Yahoo')],
-            [sg.Text('Aceita Cartao')]
-            [sg.Radio('Sim','Cartoes'), sg.Radio('Não','Cartoes')],
+            [sg.Text('Nome', size=(5,0)),sg.Input(size=(15,0),key='nome')],
+            [sg.Text('Idade',size=(5,0)),sg.Input(size=(15,0),key='idade')],
+            [sg.Text('Quais Provedores de e-mail são aceitos: ')],
+            [sg.Checkbox('Gmail',key='gmail'), sg.Checkbox('outlook',key='outlook'), sg.Checkbox('Yahoo',key='yahoo')],
+            [sg.Text('Aceita Cartao')],
+            [sg.Radio('Sim','Cartoes',key='aceitacartao'), sg.Radio('Não','Cartoes',key='naoaceitacartao')],
             [sg.Button('Enviar Dados')],
         ]
         # janela
-        janela = sg.Window("Dados do usuario").layout(layout)
+        self.janela = sg.Window("Dados do usuario").layout(layout)
         # Extrair os dados da tela
-        self.button, self.values = janela.Read()
 
     def Iniciar(self):
-        print (self.values)
+        while True:
+            self.button, self.values = self.janela.Read()
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceitacartao']
+            nao_aceita_cartao = self.values['naoaceitacartao']
+
+            print (f'nome: {nome}')
+            print (f'idade: {idade}')
+            print (f'Aceita gmail: {aceita_gmail}')
+            print (f'Aceita outlook: {aceita_outlook}')
+            print (f'Aceita yahoo: {aceita_yahoo}')
+            print (f'Aceita Cartao: {aceita_cartao}')
+            print (f'Nao aceita Cartao: {nao_aceita_cartao}')
 
 tela = telaPython()
 tela.Iniciar()
