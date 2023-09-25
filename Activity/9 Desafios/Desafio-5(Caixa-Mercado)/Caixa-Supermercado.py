f3f67231dd39e7f4a410611ec02efa5297751aac
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-09-25 05:51:26 -0400

    up

---
+++ Activity/9 Desafios/Desafio-5(Caixa-Mercado)/Caixa-Supermercado.py
@@ -0,0 +1,4 @@
+# 5 - Desenvolva um sistema simulando um caixa de supermercado em Python, utilizando 
+# Programação Orientada a Objetos (POO). 
+# O sistema deve permitir a realização de compras, cálculo de total e troco. 
+
