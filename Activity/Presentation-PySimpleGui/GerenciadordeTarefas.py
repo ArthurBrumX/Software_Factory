0747a2e398563176a40a44b36004719052a80f6e
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-09-25 07:37:12 -0400

    up

---
+++ Activity/Presentation-PySimpleGui/GerenciadordeTarefas.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+#Crindo o layout
+def criar_janela_inicial():
+    sg.theme('DarkBlue4')
+    linha = [
+        [sg.Checkbox(''),sg.Input('')]
+    ]
+
+    layout = [
+        [sg.Frame('Tarefas',layout=linha, key = 'container')],
+        [sg.Button('Nova Tarefa'),sg.Button('Resetar')]
+
+    ]
+
+    return sg.Window('Todo List',layout=layout,finalize=True)
+
+#Criando a janela
+
+janela = criar_janela_inicial()
+
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Nova Tarefa':
+        janela.extend_layout(janela['container'], [sg.Checkbox(''), sg.Input('')])
+    elif event == 'Resetar':
+        janela.close()
+        janela = criar_janela_inicial()
