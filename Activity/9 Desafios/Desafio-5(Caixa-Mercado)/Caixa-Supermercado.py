935265ba3198e0a8233734abc81617cd99d09b10
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-10-09 18:17:15 -0400

    up

--- Activity/9 Desafios/Desafio-5(Caixa-Mercado)/Caixa-Supermercado.py
+++ Activity/9 Desafios/Desafio-5(Caixa-Mercado)/Caixa-Supermercado.py
@@ -2,19 +2,69 @@
 # Programação Orientada a Objetos (POO). 
 # O sistema deve permitir a realização de compras, cálculo de total e troco. 
 
-from ClassesMercado import produto
-from ClassesMercado import caixa
-
-print("Super Mercado Cubiculos!")
+from ClassesMercado import novaCompra
 
 comprar = input("Deseja Comprar [S/N]: ")
 
 carrinho = []
 
-listaProdutos = produto()
-
 while comprar == "S" or comprar == "s":
-    carrinho.append(listaProdutos.produto())
+
+    if comprar == "S" or comprar == "s":
+        novaCompra = novaCompra()
+
+        itens = input("Digite o Nome do Item: ")
+
+        if itens in listaDeItens:
+            carrinho.append(listaDeItens)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+
+# from ClassesMercado import produto
+# from ClassesMercado import caixa
+
+# print("Super Mercado Cubiculos!")
+
+# comprar = input("Deseja Comprar [S/N]: ")
+
+# carrinho = []
+
+# listaProdutos = produto()
+
+# while comprar == "S" or comprar == "s":
+#     carrinho.append(listaProdutos.produto())
 
 
     
--- Activity/9 Desafios/Desafio-5(Caixa-Mercado)/ClassesMercado.py
+++ Activity/9 Desafios/Desafio-5(Caixa-Mercado)/ClassesMercado.py
@@ -2,42 +2,137 @@
 # Programação Orientada a Objetos (POO). 
 # O sistema deve permitir a realização de compras, cálculo de total e troco. 
 
-class produto: # controlar os produtos
-    def __init__(self):
-        self.nome = input("Digite o Nome Do Produto")
-        self.valor = input("Digite o Valor do produto")
-
-class caixa: # controla saida de produtos
+class novaCompra:
     def __init__(self):
         pass
+        self.listaDeItens = [
+            [Arroz = 15],
+            [Feijao = 5.50],
+            [Açucar = 9.90],
+            [Macarrao = 3.50],
+            [Suco = 0.99],
+            [Refrigerante = 5.50]
+        ]
 
-    def adcionarAoCarrinho(self,produto,quantidade):
-        self.carrinho.append((produto,quantidade))
+    def private
 
-    def passarNoCaixa(self):
-        valorCompra = 0
-        for produto, quantidade in self.carrinho:
-            valorCompra += produto.preco * quantidade
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ==============================================================================
+
+# class novaCompra:
+#     def __init__(self):
+#         self.carrinho = []
+#         self.produtosDoMercado = [
+#             ["Arroz", 15],
+#             ["Feijao", 5.50],
+#             ["Açucar", 9.90],
+#             ["Macarrao", 3.50],
+#             ["Suco", 0.99],
+#             ["Refrigerante", 5.50]
+#         ]
+#     class caixa: # controla saida de produtos
+#         def __init__(self):
+#             pass
+
+#         def adcionarAoCarrinho(self,produto,quantidade):
+#             self.produto = input("Qual o Nome Do Produto: ")
+#             self.quantidade = int(input("Quantos Vai Querer: "))
+#             if produto in self.produtosDoMercado:
+#                 self.carrinho.append((produto))
+
+#         def passarNoCaixa(self):
+#             valorCompra = 0
+#             if produto in self.produtosDoMercado:
+#                 valorCompra += self.produtosDoMercado[1] * quantidade
+            
+#             return valorCompra
+
+#         def pagar(self, valorPago):
+#             valorCompra = self.passarNoCaixa()
+#             troco = valorPago - valorCompra
+#             if troco >= 0:
+#                 self.imprimirRecibo(valorCompra, troco)
+#                 self.carrinho = []
+#             else:
+#                 print("Valor pago insuficiente, adicione mais dinheiro")
+            
+#         def imprimir_recibo(self, total, troco):
+#             print("Recibo de Compra")
+#             print("================")
+#             for produto, quantidade in self.produtos_comprados:
+#                 print(f"{produto.nome}: {quantidade} x R${produto.preco:.2f}")
+#             print("================")
+#             print(f"Total: R${total:.2f}")
+#             print(f"Troco: R${troco:.2f}")
+#             print("================")
+
+
+# ==============================================================================
+
+
+# class produto: # controlar os produtos
+#     def __init__(self):
+#         self.nome = input("Digite o Nome Do Produto")
+#         self.valor = input("Digite o Valor do produto")
+
+# class caixa: # controla saida de produtos
+#     def __init__(self):
+#         self.carrinho = []
+
+#     def adcionarAoCarrinho(self,produto,quantidade):
+#         self.carrinho.append((produto,quantidade))
+
+#     def passarNoCaixa(self):
+#         valorCompra = 0
+#         for produto, quantidade in self.carrinho:
+#             valorCompra += produto.preco * quantidade
         
-        return valorCompra
+#         return valorCompra
 
-    def pagar(self, valorPago):
-        valorCompra = self.passarNoCaixa()
-        troco = valorPago - valorCompra
-        if troco >= 0:
-            self.imprimirRecibo(valorCompra, troco)
-            self.carrinho = []
-        else:
-            print("Valor pago insuficiente, adicione mais dinheiro")
+#     def pagar(self, valorPago):
+#         valorCompra = self.passarNoCaixa()
+#         troco = valorPago - valorCompra
+#         if troco >= 0:
+#             self.imprimirRecibo(valorCompra, troco)
+#             self.carrinho = []
+#         else:
+#             print("Valor pago insuficiente, adicione mais dinheiro")
         
-    def imprimir_recibo(self, total, troco):
-        print("Recibo de Compra")
-        print("================")
-        for produto, quantidade in self.produtos_comprados:
-            print(f"{produto.nome}: {quantidade} x R${produto.preco:.2f}")
-        print("================")
-        print(f"Total: R${total:.2f}")
-        print(f"Troco: R${troco:.2f}")
-        print("================")
+#     def imprimir_recibo(self, total, troco):
+#         print("Recibo de Compra")
+#         print("================")
+#         for produto, quantidade in self.produtos_comprados:
+#             print(f"{produto.nome}: {quantidade} x R${produto.preco:.2f}")
+#         print("================")
+#         print(f"Total: R${total:.2f}")
+#         print(f"Troco: R${troco:.2f}")
+#         print("================")
 
 
