229bfcacf543206df6fedf49f3c5c2b68144f093
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-12-06 09:50:30 -0300

    up

---
+++ AZ-coin/Includes/header.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("./AZ-coin/Includes/menuLateral/Home.php");
+    require_once("./AZ-coin/Includes/menuLateral/MenuGestor.php");
+    require_once("./AZ-coin/Includes/menuLateral/Saldos.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+          <meta charset="utf-8">
+          <title>Index</title>
+          <link rel="stylesheet" href="../Menu Lateral/Menu.css">
+          <link rel="stylesheet" href="Styles/General_Screen_7.css">
+          <!-- <link rel="stylesheet" href="Styles/Screen-14-15-16_6.css"> -->
+          <!-- <link rel="stylesheet" href="Styles/Screen-23_6.css"> -->
+          <!-- <link rel="stylesheet" href="Styles/Screen-cell_6.css"> -->
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tomorrow">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow">
+          <link rel="stylesheet" href="modal/modal.css">
+          <script src="modal/modal.js" defer></script>
+    </head>
