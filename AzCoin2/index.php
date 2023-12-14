26a53380c3d7cc12b6feb995b87231ec7ec97b54
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-12-14 01:21:03 -0400

    up

--- AzCoin2/index.php
+++ AzCoin2/index.php
@@ -1,23 +1,7 @@
 <?php
-<<<<<<< HEAD
 
     require_once('MenuGestor.php');
     require_once('Header.php');
     require_once('Body.php');
 
-?>
-=======
->>>>>>> 0ea5b026d80e7cb02e3478d209fedea1df545231
-
-// Assim, quando estiver dentro do mesmo  arquivo
-    // require_once('MenuGestor.php');
-    // require_once('Header.php');
-    // require_once('Body.php');
-
-// Assim quando estiver em outra pasta
-require __DIR__.'/vendor/autoload.php';
-
-include __DIR__.'/includes/MenuGestor.php';
-include __DIR__.'/includes/Header.php';
-include __DIR__.'/includes/Body.php';
 ?>
