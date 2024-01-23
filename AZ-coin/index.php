cfb9456b24ddf96dcee9729baea817290e76c20b
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-01-23 12:32:49 -0300

    up

--- AZ-coin/index.php
+++ AZ-coin/index.php
@@ -1,5 +1,7 @@
 <?php
-require_once("./AZ-coin/Includes/header.php");
+echo "Ola!";
+// require_once("..\AZ-coin\index.php");
+// require_once("./AZ-coin/Includes/header.php");
 // require_once("./AZ-coin/Includes/body.php");
 // require_once("./AZ-coin/Includes/footer.php");
 ?>
