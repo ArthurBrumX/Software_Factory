00bcb350a37a893f60d736aac882c34d376cf333
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-01-25 04:55:00 -0400

    up

--- TesteAzSoftware/index.php
+++ TesteAzSoftware/index.php
@@ -1,3 +1,5 @@
 <?php 
-    include __DIR__.'\Includes\principal\header.php';
-    include __DIR__.'\Includes\principal\body.php';
+    include __DIR__.'/Includes/menuLateral/header.php';
+    include __DIR__.'/Includes/menuLateral/body.php';
+    include __DIR__.'/Includes/principal/header.php';
+    include __DIR__.'/Includes/principal/body.php';
